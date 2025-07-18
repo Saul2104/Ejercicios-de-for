12c2780d368172ed0b6c5ae2ea0aff3df02ce127
Saul2104 <saulolmos742@gmail.com>
2025-07-17 20:54:49 -0500

    Add files via upload

---
+++ eje4for.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Números pares del 1 al 50:\n";
+    for (int i = 2; i <= 50; i += 2) {
+    cout << i << std::endl;
+    }
+    return 0;
+}
