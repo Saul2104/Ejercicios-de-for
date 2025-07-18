0d86168b702fafd553615f31b13920d9801e1727
Saul2104 <saulolmos742@gmail.com>
2025-07-17 21:05:09 -0500

    Add files via upload

---
+++ eje10for.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int n, a = 0, b = 1, siguiente;
+
+    cout << "Ingrese la cantidad de terminos de la serie Fibonacci: ";
+    cin >> n;
+
+    cout << "Serie Fibonacci: ";
+
+    for (int i = 1; i <= n; i++) {
+        cout << a << " ";
+        siguiente = a + b;
+        a = b;
+        b = siguiente;
+    }
+
+    cout << endl;
+    return 0;
+}
